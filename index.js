3e06c44dcdff9603b824b15729c5ab3374f4512f
niranjankumarbb <niranjan32@gmail.com>
2021-01-30 02:04:30 +0530

    updated

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ app.use(express.json())
  const routes = require('./config/routes')
   app.use('/',routes)
  
-if(process.env.NODE_ENV== 'production'){
-    app.use(express.static(path.join(__dirname,"client/build"))) 
-   app.get("*",(req,res) => { 
-   res.sendFile(path.join(__dirname + "/client/build/index.html")) 
-}) 
+  if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
 }
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(port,()=>{
     console.log('Listening on port', port)
